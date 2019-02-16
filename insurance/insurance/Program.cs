5f269cd40d3fc83375779ace72642b3e87f1cfbe
Jeremy Stewart <jerstew@gmail.com>
2019-02-16 12:08:03 -0800

    console app for Tech Academy, see if you are qualified for car insurance

---
+++ insurance/insurance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace insurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("What is your age ? ");
+            int age = Convert.ToByte(Console.ReadLine());
+            Console.Write("\nHave you ever had a DUI (true or false) ");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("\nHow many speeding tickets do you have? ");
+            int tickets = Convert.ToByte(Console.ReadLine());
+
+            Console.Write("\nQualified? " + Convert.ToString((age > 15) && (dui == false) && (tickets <= 3)));
+            Console.Read();
+        }
+    }
+}
