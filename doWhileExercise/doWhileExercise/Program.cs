75e9949f6621f42aad68e3769cbe9037c550c6f1
Jeremy Stewart <jerstew@gmail.com>
2019-02-18 23:23:57 -0800

    Do/While Exercies for The Tech Academy

---
+++ doWhileExercise/doWhileExercise/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhileExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mycolor = "start";
+
+            Console.Write("Guess my favorite color: ");
+            mycolor = Console.ReadLine();
+
+            while (mycolor != "orange")
+            {
+                Console.Write("Wrong! Try again: ");
+                mycolor = Console.ReadLine();
+            }
+            Console.WriteLine("Yeah, orange IS the best color!");
+
+            Console.Write("Guess my second favorite: ");
+
+            do
+            {
+                mycolor = Console.ReadLine();
+                switch (mycolor)
+                {
+                    case "orange" : Console.Write("Orange is my favorite! You must have a short memory.\nTry again: ");
+                        break;
+                    case "green": Console.Write("I like green too, but not as much as orange and... ");
+                        break;                   
+                    case "blue":
+                        break;
+                    default : Console.Write("Wrong! Try again: ");
+                        break;
+                }
+                
+            } while (mycolor != "blue");
+
+
+            Console.WriteLine("Wow! You must be a psychic!");
+            Console.ReadLine();
+
+        
+        }
+    }
+}
---
+++ loops/loops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGuessed = false;
+            int number;
+            while (!isGuessed)
+            {
+                Console.WriteLine("Guess a number?");
+                number = Convert.ToInt32(Console.ReadLine());
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        break;
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
