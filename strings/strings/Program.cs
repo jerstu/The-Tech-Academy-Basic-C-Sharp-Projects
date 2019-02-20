e61e08e81cc543950a8aa5f21f4babf9c34ce89b
Jeremy Stewart <jerstew@gmail.com>
2019-02-19 21:50:54 -0800

    strings and stringbuilder

---
+++ strings/strings/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jeremy";
+            //string quote = "The man said, \"Hello\", Jeremy. \n Hello on a new line. \n\t Hello on a tab.";
+            //string fileName = "C:\\Users\\Jeremy";
+
+            //bool trueOrFalse = name.Contains("e");
+            //Console.WriteLine(trueOrFalse);
+            //trueOrFalse = name.EndsWith("y");
+            //Console.WriteLine(trueOrFalse);
+            //int length = name.Length;
+            //Console.WriteLine("Name Length of " + name + " is " + length);
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(name);
+            //Console.WriteLine(quote);
+            //Console.WriteLine(fileName);
+            //Console.ReadLine();
+
+            //string name2 = "Jeremy";
+            //name2 = "Joe";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My Name is Jeremy.");
+            sb.Append(" I live at 3545 N Borthwick Ave.");
+
+            
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        
+        }
+    }
+}
---
+++ stringsExercise/stringsExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "My name is Jeremy. ";
+            string string2 = "I have too many coffee gadgets. ";
+            string string3 = "Maybe I should get rid of some. \n";
+
+            Console.WriteLine(string1 + string2 + string3);
+
+            StringBuilder paragraph = new StringBuilder("\tI don't think it's a problem though. ");
+            paragraph.Append("Whenever I move, I have a big box labeled ");
+            string string4 = "\"coffee\". ";            
+            paragraph.Append(string4.ToUpper());
+            paragraph.Append("It gets bigger and heavier each time. ");
+            paragraph.Append("There is a coffee maker for each day of the week. ");
+            paragraph.Append("I can roast coffee at home with an electric roaster. ");
+            paragraph.Append("Maybe I'm taking it too far when I roast over the campfire.");
+
+            Console.WriteLine(paragraph);
+
+            Console.ReadLine();
+         
+        }
+    }
+}
