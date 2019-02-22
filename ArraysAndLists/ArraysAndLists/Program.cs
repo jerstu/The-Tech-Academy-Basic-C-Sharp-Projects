275568315b55f4699df2c6bc7394a5b36441b185
Jeremy Stewart <jerstew@gmail.com>
2019-02-21 19:56:06 -0800

    Arrays, lists and arrays exercise

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] numArray = new int[5];
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 200;
+        numArray[4] = 5000;
+
+        int[] numArray1 = new int[] { 3, 7, 17, 175, 3333 };
+
+        int[] numArray2 = { 6, 12, 24, 48, 96, 192 };
+
+        int n = 0;
+        while(n < numArray.Length)
+        {
+            Console.WriteLine(numArray[n]);
+            Console.WriteLine(numArray1[n]);
+            Console.WriteLine(numArray2[n]);
+            n++;
+        }
+        Console.ReadLine();
+
+        List<int> intlist = new List<int>();
+        intlist.Add(4);
+        intlist.Add(10);
+
+        Random rand = new Random();
+        n = 0;
+        for (n = 0; n < 5; n++)
+            intlist.Add(rand.Next());
+        n = 0;
+        while (n < intlist.Count)
+        {
+            Console.WriteLine(intlist[n]);
+            n++;
+        }
+        Console.ReadLine();
+        
+    }
+}
+
---
+++ arrayExercise/arrayExercise/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] funnyQuotes = {    "A day without sunshine is like, you know, night. \n\t-Steve Martin",
+                                        "Behind every great man is a woman rolling her eyes. \n\t-Jim Carrey",
+                                        "If love is the answer, could you please rephrase the question? \n\t-Lily Tomlin",
+                                        "Happiness is having a large, loving, caring, close-knit family in another city. \n\t-George Burns",
+                                        "If you're going to do something tonight that you'll be sorry for tomorrow morning, sleep late. \n\t-Henny Youngman",
+                                        "Age is something that doesn't matter, unless you are a cheese. \n\t-Luis Bunuel",
+                                        "To succeed in life, you need three things: a wishbone, a backbone and a funny bone. \n\t-Reba McEntire",
+                                        "Do not take life too seriously. You will never get out of it alive. \n\t-Elbert Hubbard",
+                                        "No man has a good enough memory to be a successful liar. \n\tAbraham Lincoln"
+            };
+
+            bool display = true;
+            int selected;
+
+            while (display)
+            {
+                Console.Write("Select a index, 0 - " + funnyQuotes.Length + ", for a somewhat funny, slightly inspiration quote: ");
+
+                selected = Convert.ToInt16(Console.ReadLine());
+                if (selected >= 0 && selected < funnyQuotes.Length)
+                {
+                    Console.WriteLine(funnyQuotes[selected]);
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid choice.");
+
+                };
+                Console.WriteLine("Press 'y' to try again");
+                string tryAgain = Console.ReadLine();
+                if (tryAgain != "y")
+                {
+                    display = false;
+                };
+
+            };
+
+            int[] intArray = { 2, 51, 326, 1008, 67, 88, 21, 4721 };
+            display = true;
+
+            while (display)
+            {
+                Console.Write("Select an index, 0 - " + intArray.Length + ", of the array: ");
+
+                selected = Convert.ToInt16(Console.ReadLine());
+                if (selected >= 0 && selected < intArray.Length)
+                {
+                    Console.WriteLine(intArray[selected]);
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid choice.");
+                };
+                Console.WriteLine("Press 'y' to try again");
+                string tryAgain = Console.ReadLine();
+                if (tryAgain != "y")
+                {
+                    display = false;
+                };
+
+            };
+
+            List<string> quotes = new List<string>();
+            for (int i = 0; i < funnyQuotes.Length; i++)
+            {
+                quotes.Add(funnyQuotes[i]);
+            };
+
+            display = true;
+            while (display)
+            {
+                Console.WriteLine("Select an index 0 - " + (quotes.Count - 1) + ", to display one more quote: ");
+
+                selected = Convert.ToInt16(Console.ReadLine());
+                if (selected >= 0 && selected < quotes.Count)
+                {
+                    Console.WriteLine(quotes[selected]);
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid choice.");
+                };
+                Console.WriteLine("Press 'y' to try again");
+                string tryAgain = Console.ReadLine();
+                if (tryAgain != "y")
+                {
+                    display = false;
+                };
+
+            };
+           
+            
+            
+        }
+
+
+
+
+          
+    }
+}
