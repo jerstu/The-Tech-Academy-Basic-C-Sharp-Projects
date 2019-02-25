ae3347f8c707c7aec6db588a5ee06b54b53c476b
Jeremy Stewart <jerstew@gmail.com>
2019-02-24 22:12:45 -0800

    exception handling exercise

---
+++ exceptionHandling/exceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> intList = new List<int> { 1, 3, 4, 22, 33, 55, 75, 101, 240, 6000 };
+                Console.Write("Divide by: ");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int integer in intList)
+                {
+                    Console.WriteLine(integer + " / " + divisor + " = " + integer / divisor + " carry " + integer%divisor);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Cannot divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Program has continued to execute.");
+            Console.ReadLine();
+        }
+    }
+}
