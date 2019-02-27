f93cd7c224ce29df2de728cd8f71deec246889b9
Jeremy Stewart <jerstew@gmail.com>
2019-02-26 20:58:01 -0800

    An exercise to practice creating a class and some methods. Program uses methods that do basic math and displays results.

---
+++ methodExercise/methodExercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input an integer: ");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+
+            // create an instance of mathOperand class
+            mathOperand squareFactor = new mathOperand();
+            // Assign userInt to Operand property
+            squareFactor.Operand = userInt;
+            // pass class instance to method and assign return to variable
+            int squareResult = mathOperand.Square(squareFactor);
+            // display result
+            Console.WriteLine(userInt + " squared = " + squareResult);
+
+            mathOperand cubeFactor = new mathOperand();
+            cubeFactor.Operand = userInt;
+            int cubeResult = mathOperand.Cube(cubeFactor);
+            Console.WriteLine(userInt + " cubed = " + cubeResult);
+
+            mathOperand add1000 =  new mathOperand();
+            add1000.Operand = userInt;
+            int add1000Result = mathOperand.AddThousand(add1000);
+            Console.WriteLine(userInt + " + 1000 = " + add1000Result);
+
+
+            Console.ReadLine();
+
+
+
+
+            
+
+
+
+        }
+
+        
+    }
+}
---
+++ methodExercise/methodExercise/operation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise
+{
+    class mathOperand
+    {
+        public mathOperand()
+        {
+            Operand = 1;
+        }
+        public int Operand { get; set; }
+        
+
+        public static int Square(mathOperand factor)
+        {
+            int square = factor.Operand * factor.Operand;
+            
+
+
+            return square;
+        }
+
+        public static int Cube(mathOperand factor)
+        {
+            int cube = factor.Operand * factor.Operand * factor.Operand;
+            return cube;
+        }
+
+        public static int AddThousand(mathOperand operand) 
+        {
+            int result = operand.Operand + 1000;
+            return result;
+        }
+
+       
+    }
+
+}
