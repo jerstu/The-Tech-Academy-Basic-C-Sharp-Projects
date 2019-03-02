ccd411588045f89aef6a3bd1990fc7bc84ec1252
Jeremy Stewart <jerstew@gmail.com>
2019-03-01 19:49:10 -0800

    console app, practice with overloaded methods

---
+++ threeMethods/threeMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threeMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Overload OL1 = new Overload();
+            OL1.PassInt = 25;
+            OL1.PassInt = Overload.Method(TakeInt: OL1.PassInt);
+            Console.WriteLine("Integer: " + OL1.PassInt);
+
+            Overload OL2 = new Overload();
+            OL2.PassDec = 333.333m;
+            OL2.PassDec = Overload.Method(TakeDec: OL2.PassDec);
+            Console.WriteLine("Decimal: " + OL2.PassDec);
+
+            Overload OL3 = new Overload();
+            OL3.PassString = "123";
+            Console.WriteLine("String: " + Overload.Method(TakeString: OL3.PassString));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ threeMethods/threeMethods/overload.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threeMethods
+{
+    public class Overload
+    {
+        public Overload()
+        {
+            PassInt = 1;
+            PassDec = 1.0m;
+            PassString = "1";
+        }
+        public int PassInt { get; set; }
+        public decimal PassDec { get; set; }
+        public string PassString { get; set; }
+
+        public static int Method(int TakeInt)
+        {
+            TakeInt = TakeInt + 3456;
+            return TakeInt;
+        }
+
+        public static decimal Method(decimal TakeDec)
+        {
+            TakeDec = TakeDec + 55.55m;
+            return TakeDec;
+        }
+
+        public static int Method(string TakeString)
+        {
+            int StringNum = Convert.ToInt32(TakeString);
+            StringNum = StringNum * 40;
+            return StringNum;
+        }
+
+    }
+}
