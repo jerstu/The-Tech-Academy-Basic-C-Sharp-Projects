8578671d9f8ac3c199b51328e0a8963f5140e9b8
Jeremy Stewart <jerstew@gmail.com>
2019-03-05 21:41:11 -0800

    practice with output parameters, overload methods and static class type

---
+++ moreMethods2/moreMethods2/Llama.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethods2
+{
+    public class Llama
+    {
+        public Llama(string name)
+        {
+            Name = name;
+            Number = 1;
+        }
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public void Spit() // Llamas stick their tongue out and spit when they are unhappy with you.
+        {
+            Console.WriteLine(Number / 2);
+            Console.ReadLine();
+        }
+
+        public void Humm(out string string1, out string string2) //Apparently they like to humm too.
+        {
+            Console.WriteLine("Input Text: ");
+            string1 = Console.ReadLine();
+
+            Console.WriteLine("Input More Text: ");
+            string2 = Console.ReadLine();
+        }
+
+        public void Humm(out int Num1, out int Num2)
+        {
+            Num1 = Number * 5;
+            Num2 = Number * Number;
+        }
+    }
+}
---
+++ moreMethods2/moreMethods2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Llama Fuzzy = new Llama("Fuzzy");
+
+            Console.WriteLine("Feed {0} a number: ", Fuzzy.Name );
+            Fuzzy.Number = (Convert.ToInt32(Console.ReadLine()));
+            Fuzzy.Spit();
+
+            Fuzzy.Humm(out int number1, out int number2);
+            Console.WriteLine("{0} {1}", number1, number2);
+            Console.ReadLine();
+
+            Fuzzy.Humm(out string myString1, out string myString2);
+            Console.WriteLine(myString1 + " " + myString2);
+            Console.ReadLine();
+
+        }
+    }
+}
