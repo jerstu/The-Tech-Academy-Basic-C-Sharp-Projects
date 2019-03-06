1d31c49d5339b7134174af8fa7b53949d40df8a5
Jeremy Stewart <jerstew@gmail.com>
2019-03-05 19:41:08 -0800

    Practice with option parameters

---
+++ optionalParameter/optionalParameter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OPClass OP1 = new OPClass() { };
+
+            Console.Write("First Number: ");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second Number (optional): ");
+            try
+            {
+                var Num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Result: " + OPClass.OPMethod(Num1, Num2));
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Result: " + OPClass.OPMethod(Num1));
+                Console.ReadLine();
+            }
+
+           
+
+          
+
+        }
+    }
+}
---
+++ optionalParameter/optionalParameter/opClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParameter
+{
+    public class OPClass
+    {
+        public OPClass()
+        {
+            Num1 = 1;
+            Num2 = 2;
+        }
+        public int Num1 { set; get; }
+        public int Num2 { set; get; }
+
+        public static int OPMethod(int Int1, int Int2 = 2)
+        {
+            return Int1 * Int2;            
+        }
+
+
+
+    }
+
+    
+}
