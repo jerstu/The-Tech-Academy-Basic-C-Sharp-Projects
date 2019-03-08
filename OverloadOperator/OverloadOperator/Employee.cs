ed393a9dc83abcb0552f45799dff8e93ec84613b
Jeremy Stewart <jerstew@gmail.com>
2019-03-07 22:27:58 -0800

    practice with overload operator method

---
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        
+        public Employee(int id, string fn, string ln)
+        {
+            Id = id;
+            FirstName = fn;
+            LastName = ln;            
+        }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+
+        // Not really sure yet what this is all about
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+    }
+}
---
+++ OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(1, "Jeremy", "Stewart");
+            Employee emp2 = new Employee(1, "Jeremy", "Stuart");
+            Employee emp3 = new Employee(2, "Pepe", "Le Pew");            
+
+            Console.WriteLine("Are {0} {1} and {2} {3} the same person? " + (emp1 == emp2), emp1.FirstName, emp1.LastName, emp2.FirstName, emp2.LastName);
+            Console.ReadLine();
+
+            Console.WriteLine("Are {0} {1} and {2} {3} the different people? " + (emp1 != emp3), emp1.FirstName, emp1.LastName, emp3.FirstName, emp3.LastName);
+            Console.ReadLine();
+
+            Console.WriteLine("Are {0} {1} and {2} {3} the same person? " + (emp1 == emp3), emp1.FirstName, emp1.LastName, emp3.FirstName, emp3.LastName);
+            Console.ReadLine();
+        }
+    }
+}
