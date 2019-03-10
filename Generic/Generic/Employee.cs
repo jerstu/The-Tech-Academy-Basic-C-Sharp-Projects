274387799fa7d3bd28a66ff4cc500d9200c8c2cd
Jeremy Stewart <jerstew@gmail.com>
2019-03-10 15:33:52 -0700

    using a class with a generic data type

---
+++ Generic/Generic/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class Employee<T>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<T> Things { get; set; }
+
+        public Employee(string fn, string ln, List<T> things)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Things = things;
+        }
+
+        public void ListThings()
+        {
+            Console.WriteLine("{0} {1} has:", FirstName, LastName);
+            foreach (var thing in Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+
+    }
+}
---
+++ Generic/Generic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee<string> jeremy = new Employee<string>("Jeremy", "Stewart", new List<string> { "phone", "wallet", "keys" });
+            Employee<int> pepe = new Employee<int>("Pepe", "Le Pew", new List<int> { 1, 2, 4, 7, 90, 1104 });
+
+            jeremy.ListThings();
+            pepe.ListThings();
+            Console.ReadLine();
+        }
+    }
+}
