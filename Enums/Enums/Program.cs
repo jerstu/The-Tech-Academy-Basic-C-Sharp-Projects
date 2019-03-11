33bb9891da228ef9348767fbd04dfc3ec0b180ee
Jeremy Stewart <jerstew@gmail.com>
2019-03-10 23:05:15 -0700

    Practice with enum types and enum parsing.

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            
+            Console.WriteLine("Enter the current day of the week:");
+            
+            for (int t = 1; t < 3 ;t++)
+            {
+                try
+                {
+                    DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+                    if ((int)day == (int)today.DayOfWeek)
+                    {
+                        Console.WriteLine("Yes, today is {0}!", day);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No, it's not {0}!", day);
+                    }
+                    
+                    break;
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Please enter an actual day of the week: ");
+                }
+            }
+            Console.ReadLine();
+            
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
