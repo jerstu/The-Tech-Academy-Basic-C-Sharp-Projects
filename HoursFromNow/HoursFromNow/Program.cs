3ad825e18bbf8078ce02f0c2ff13b94379f3e111
Jeremy Stewart <jerstew@gmail.com>
2019-03-22 14:23:50 -0700

    Working with datetime. Adds input as hours to datetime and diplays to console.

---
+++ HoursFromNow/HoursFromNow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoursFromNow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In {0} hours it will be {1}.", hours, DateTime.Now.AddHours(hours).ToShortTimeString());
+            Console.Read();
+        }
+    }
+}
