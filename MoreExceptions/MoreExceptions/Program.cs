58171f01740bec551bfd43dbc9e90e83dee9a22f
Jeremy Stewart <jerstew@gmail.com>
2019-03-23 19:13:53 -0700

    Practice with Exception Handling and custom exceptions.

---
+++ MoreExceptions/MoreExceptions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int age;
+            Console.Write("Enter your age: ");
+            try
+            {
+                age = Convert.ToInt16(Console.ReadLine());
+                if (age == 0) throw new ZeroException();
+                if (age < 0) throw new NegativeException();
+            }
+            catch (ZeroException)
+            {
+                Console.WriteLine("Your age cannot be zero...");
+                Console.ReadLine();
+                Console.WriteLine("Unless you are a baby. If you are a baby, how are you reading and using a computer already??");
+                Console.ReadLine();
+                return;
+            }
+            catch (NegativeException)
+            {
+                Console.WriteLine("Your age definetly cannot be negative.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong, did you enter something incorrectly?");
+                Console.ReadLine();
+                return;
+            }
+            bool validAnswer = false;
+            while (!validAnswer)
+            {
+                Console.Write("And have you had a birthday yet this year? (y or n): ");
+                string answer = Console.ReadLine();
+                if (answer == "y") validAnswer = true;
+                else if (answer == "n")
+                {
+                    validAnswer = true;
+                    age += 1;
+                }
+                else Console.WriteLine("Please enter y or n!");
+            }
+            Console.WriteLine("You were born in {0}.", DateTime.Now.Year - age);
+            Console.ReadLine();
+        }
+    }
+}
